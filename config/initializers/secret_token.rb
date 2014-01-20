091ee98b1c24138445c50ca0629b0cd1c6f25659
Jerrold Thompson <jet@whidbey.com>
2014-01-19 22:18:50 -0800

    Zurb Foundation testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '83894fed60eb6fdbe1b96e8ccc84cb2963cf85a1d5078191cfca34a8ff65dccf5f839d429af54fd5a826ff2ea5267dfe5963f0fdb92125f3af500d5d054bfe2a'
